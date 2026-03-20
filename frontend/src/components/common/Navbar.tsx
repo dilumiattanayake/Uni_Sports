11a293394607d89d56f9163e379ff4b1f725f8e0
Dinuja <dinujaubeysinghe0@gmail.com>
2026-03-20 09:39:08 +0530

    Update Navbar.tsx file

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Unisport from "@/assets/UniSport.jpg";
 
 const Navbar: React.FC = () => {
 
@@ -9,9 +10,30 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
 
         {/* Logo */}
-        <Link to="/" className="text-2xl font-bold text-orange-500">
-          UniSports
+        
+          <Link to="/" className="text-2xl ">
+           <img 
+              src={Unisport} 
+              alt="UniSport Logo" 
+              className="h-8 w-auto" 
+           />
+          
         </Link>
+        
+        
+
+        {/* Menu */}
+        <div className="space-x-6 hidden md:flex">
+
+          <a href="#features" className="hover:text-orange-400">Features</a>
+          <a href="#how-it-works" className="hover:text-orange-400">How it works</a>
+          <a href="#sports" className="hover:text-orange-400">Sports</a>
+          <a href="#events" className="hover:text-orange-400">Events</a>
+          <a href="#merchandise" className="hover:text-orange-400">Merchandise</a>
+          
+         
+          
+        </div>
 
         {/* Auth Buttons */}
         <div className="space-x-4">
